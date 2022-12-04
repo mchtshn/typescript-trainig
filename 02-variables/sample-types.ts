5df406749d2dce245f68b11408f077ad0c9fa51b
TURKCELL\EXT02D47957 <email@turkcell.com.tr>
2022-12-04 20:26:16 +0300

    variables added

---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,16 @@
+let found : boolean = true;
+
+let grade : number = 54.5;
+
+let firstName : string = "bla bla";
+
+let lastName : string = "bla bla";
+
+
+console.log(found);
+
+console.log("The grade is "+ grade);
+
+console.log("My first name is " + firstName);
+
+console.log(`My last name is ${lastName}`);
